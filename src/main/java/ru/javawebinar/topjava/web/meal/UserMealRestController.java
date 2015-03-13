3f13206d875daf70ed37e2a59e7f9ad01dffed82
Eugene Poddubny <L1ttleH0bb1t@yandex.ru>
2015-03-13 21:39:37 +0300

    change interfaceImpl to interface in controller

--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.LoggedUser;
 import ru.javawebinar.topjava.LoggerWrapper;
 import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.service.UserMealService;
 import ru.javawebinar.topjava.service.UserMealServiceImpl;
 
 import java.util.Date;
@@ -20,7 +21,7 @@ public class UserMealRestController {
     private LoggerWrapper LOG = LoggerWrapper.get(UserMealRestController.class);
 
     @Autowired
-    private UserMealServiceImpl service;
+    private UserMealService service;
 
     public UserMeal create(UserMeal userMeal) {
         LOG.debug("create " + userMeal);
